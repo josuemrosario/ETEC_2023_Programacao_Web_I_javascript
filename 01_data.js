00ff40bdf75faa14dc36f8a3d8519176db6acf4d
josue Micaroni do Rosario <josuemrosario@yahoo.com.br>
2023-06-07 20:39:34 -0300

    Classe Date()

---
+++ 01_data.js
@@ -0,0 +1,34 @@
+//rodar com node data.js
+//Conceitos: 
+// Objeto Data, 
+
+//Crie uma const data_atual usando o objeto Date e mostre na console
+
+//Crie outra const dataN com a primeira data Date(0)
+//Vá aumentando o zero gradativamente e observe o resultado
+
+//Crie variaveis representando um segundo(1000 milisegundos), um minuto, uma hora e um dia em milisegundos
+//crie um objeto date e experimente usar as variaveis como parametro
+// umSegundo = 1000
+// umMinuto = umSegundo * 60
+// umaHora = umMinuto * 60
+// umDia = umaHora * 24
+
+
+//crie sua data de nascimento usando Date(yyyy, mm, dd, hh, mm, ss, ml)
+//Obs: Nao se esqueca que os numeros comecam em zero
+
+//crie sua data de nascimento usando Date(string)
+//Obs: Neste caso nao comeca com zero
+//Obs: Necessario colocar a hora
+
+//A partir da data de nascimento obtenha o dia(getDate) mes e ano separadamento
+// console.log(dataNascimento.getDate().toString())
+
+//Obtenha o numero do dia semana com getDay()
+// 0 - domingo
+
+
+//Obtenha a data formatada com a locale do computador
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/toLocaleTimeString
+//console.log(dataNascimento.toLocaleDateString('pt-BR'))
